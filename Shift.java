5fb7bebeccdbc4ebecc78bfa0e2ed822d06117f8
Oscar <theosgrouch@gmail.com>
2022-03-28 21:58:09 -0400

    Changed Time.java from record to class for functionality

--- Shift.java
+++ Shift.java
@@ -1,5 +1,7 @@
 /**
- * Class representing a shift worked for RIT Dining
+ * Class representing a shift worked for RIT Dining.
+ * Keeps track of the day worked, time clocked in, time clocked out,
+ * place worked and the job worked.
  */
 public class Shift {
 	/** Day worked */
@@ -9,6 +11,8 @@ public class Shift {
 	private Time in;
 	/** Time clocked out */
 	private Time out;
+	/** Total time worked */
+	private Time total;
 
 	/** Place worked */
 	private Location loc;
@@ -40,6 +44,8 @@ public class Shift {
 		this.loc = selectLocation(locChoice);
 		// set the job worked based on the choice given
 		this.job = selectJob(jobChoice);
+
+		this.total = timeDifference();
 	}
 
 	/**
@@ -79,6 +85,7 @@ public class Shift {
 		}
 
 		// create the different variables for the calendar date
+		// by splitting the date given to "MM" "DD" "YYYY"
 		String[] dateSplit = calendarDate.split("/");
 		int month = Integer.parseInt(dateSplit[0]);
 		int day = Integer.parseInt(dateSplit[1]);
@@ -94,6 +101,7 @@ public class Shift {
 	 * @return Time instance with properties from entry
 	 */
 	private Time createTime(String entry) {
+		// splits entry into: "HH" "MM" "AM/PM"
 		String[] entrySplit = entry.split(":\\s+");
 		int hour = Integer.parseInt(entrySplit[0]);
 		int minutes = Integer.parseInt(entrySplit[1]);
@@ -168,5 +176,77 @@ public class Shift {
 		}
 
 		return temp;
+	}
+
+	/**
+	 * Finds the amount of time worked by taking the difference between
+	 * the clock in and clock out times and returning the difference as
+	 * a Time instance.
+	 * 
+	 * @return Time instance of the total time worked
+	 */
+	private Time timeDifference() {
+		int hourDiff = out.getMilitaryHour() - in.getMilitaryHour();
+
+		int minutesDiff;
+		if (out.getMinutes() < in.getMinutes()) {
+			// if clock out minutes is less than clock in minutes,
+			// then a full hour was not completed,
+			// therefore decrease the hours worked by one and calculate the minutes
+			--hourDiff;
+			minutesDiff = (60 + out.getMinutes()) - in.getMinutes();
+		} else if (out.getMinutes() > in.getMinutes()) {
+			// if clock out minutes is greater than clock in minutes,
+			// then just calculate their difference
+			minutesDiff = out.getMinutes() - in.getMinutes();
+		} else {
+			// else the minutes are the same and there is no difference
+			minutesDiff = 0;
+		}
+
+		// set ampm as "UNDEF" because this new instance is the difference of two times
+		return new Time(hourDiff, minutesDiff, "UNDEF");
+	}
+
+	/**
+	 * @return Date of the shift
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @return Time instance of time clocked in
+	 */
+	public Time getIn() {
+		return in;
+	}
+
+	/**
+	 * @return Time instance of time clocked out
+	 */
+	public Time getOut() {
+		return out;
+	}
+
+	/**
+	 * @return Time instance of total time worked
+	 */
+	public Time getTotal() {
+		return total;
+	}
+
+	/**
+	 * @return the place of work
+	 */
+	public Location getLoc() {
+		return loc;
+	}
+
+	/**
+	 * @return the shift worked, on paper
+	 */
+	public Jobs getJob() {
+		return job;
 	}
 }
--- Time.java
+++ Time.java
@@ -1,9 +1,76 @@
 /**
- * Record for the times clocked in and out
+ * Class representing Time, used to track clock ins, clock outs
+ * and total time worked during a shift.
  */
-public record Time(
-		int hour,
-		int minutes,
-		String ampm) {
+public class Time {
+	/** The hour in 12h format */
+	private int hour;
+	/** The hour in 24h format, useful for time math */
+	private int militaryHour;
+	/** The minute */
+	private int minutes;
+	/** AM or PM */
+	private String ampm;
 
+	/**
+	 * Create a new Time instance with the given information.
+	 * 
+	 * @param hour    the hour
+	 * @param minutes the minute
+	 * @param ampm    AM, PM or UNDEF when calculating time worked
+	 */
+	public Time(int hour, int minutes, String ampm) {
+		this.hour = hour;
+		this.minutes = minutes;
+		this.ampm = ampm;
+		this.militaryHour = calculateMilitaryHour();
+	}
+
+	/**
+	 * Converts the 12h hour into 24h military time,
+	 * if valid ampm was set.
+	 * 
+	 * @return the hour in 24h format
+	 */
+	private int calculateMilitaryHour() {
+		int temp;
+
+		if (ampm.equals("AM")) {
+			temp = hour;
+		} else if (ampm.equals("PM")) {
+			temp = hour + 12;
+		} else {
+			temp = -1;
+		}
+
+		return temp;
+	}
+
+	/**
+	 * @return the hour in 12h format
+	 */
+	public int getHour() {
+		return hour;
+	}
+
+	/**
+	 * @return the hour in 24h format
+	 */
+	public int getMilitaryHour() {
+		return militaryHour;
+	}
+
+	/**
+	 * @return the minutes
+	 */
+	public int getMinutes() {
+		return minutes;
+	}
+
+	/**
+	 * @return AM or PM
+	 */
+	public String getAmpm() {
+		return ampm;
+	}
 }
