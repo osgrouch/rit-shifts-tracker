e19c1cb3fc61eb06f9d5a2e4d6cf2a554745f87f
Oscar <theosgrouch@gmail.com>
2022-05-11 16:03:21 -0400

    Adding valueOfCode method to Job enum

--- src/work/CGShift.java
+++ src/work/CGShift.java
@@ -7,67 +7,78 @@ package src.work;
  */
 public class CGShift extends Shift {
 	/** Enum representing the jobs available at the C&G */
-	public static enum Jobs {
-		CASHIER,
-		DINING,
-		FLEX,
-		FRYER,
-		GRILLE,
-		KDS,
-		PREP,
-		SALSARITAS,
-		UTILITY
+	public static enum Job {
+		CASHIER(1),
+		DINING(2),
+		FLEX(3),
+		FRYER(4),
+		GRILLE(5),
+		KDS(6),
+		PREP(7),
+		SALSARITAS(8),
+		UTILITY(9);
+
+		/** Number representing the job worked */
+		private int code;
+
+		/**
+		 * Create a Job enum element with the given code.
+		 * 
+		 * @param code the code representing the job
+		 */
+		private Job(int code) {
+			this.code = code;
+		}
+
+		/**
+		 * @return the job's code
+		 */
+		public int getCode() {
+			return code;
+		}
+
+		/**
+		 * Find the job value of the given number by iterating through the
+		 * elements in the enum until a matching code is found,
+		 * assumes the given argument is in the valid range of [1, 9].
+		 * 
+		 * @param num the number of the job to find
+		 * @return the job
+		 */
+		public static Job valueOfCode(int num) {
+			Job value = null;
+
+			for (Job current : values()) {
+				if (num == current.getCode()) {
+					value = current;
+					break;
+				}
+			}
+
+			return value;
+		}
 	}
 
 	/** The job worked during the shift */
-	private Jobs job;
+	private Job job;
 
 	/**
-	 * Create a new CGShift and set its information by calling the parent
-	 * Shift class' constructor
+	 * Create a new CGShift and set its information by calling
+	 * the parent Shift class' constructor
 	 * 
-	 * @param args an array of strings with the different elements each representing
-	 *             a different piece of information about the shift to create
+	 * @param args an array of strings with the different elements
+	 *             each representing a different piece of information about the
+	 *             shift to create
 	 */
 	public CGShift(String[] args) {
 		super(args);
-
-		int jobChoice = Integer.parseInt(args[4]);
-		switch (jobChoice) {
-			case 1:
-				this.job = Jobs.CASHIER;
-				break;
-			case 2:
-				this.job = Jobs.DINING;
-				break;
-			case 3:
-				this.job = Jobs.FLEX;
-				break;
-			case 4:
-				this.job = Jobs.FRYER;
-				break;
-			case 5:
-				this.job = Jobs.GRILLE;
-				break;
-			case 6:
-				this.job = Jobs.KDS;
-				break;
-			case 7:
-				this.job = Jobs.PREP;
-				break;
-			case 8:
-				this.job = Jobs.SALSARITAS;
-				break;
-			default:
-				this.job = Jobs.UTILITY;
-				break;
-		}
+		this.job = Job.valueOfCode(Integer.parseInt(args[4]));
 	}
 
 	/**
 	 * @return the job worked during this shift
 	 */
-	public Jobs getJob() {
+	public Job getJob() {
 		return job;
 	}
 
--- src/work/MarketShift.java
+++ src/work/MarketShift.java
@@ -1,48 +1,78 @@
 package src.work;
 
 /**
- * Class representing a shift worked at The Market at Global Village (Market).
+ * Class representing a shift worked at
+ * The Market at Global Village (Market).
  * Extends Shift and adds jobs specific to the Market.
  */
 public class MarketShift extends Shift {
 	/** Enum representing the jobs available at the Market */
-	public static enum Jobs {
-		CASHIER,
-		STOCKER,
-		UTILITY
+	public static enum Job {
+		CASHIER(1),
+		STOCKER(2),
+		UTILITY(3);
+
+		/** Number representing the job worked */
+		private int code;
+
+		/**
+		 * Create a Job enum element with the given code.
+		 * 
+		 * @param code the code representing the job
+		 */
+		private Job(int code) {
+			this.code = code;
+		}
+
+		/**
+		 * @return the job's code
+		 */
+		public int getCode() {
+			return code;
+		}
+
+		/**
+		 * Find the job value of the given number by iterating through the
+		 * elements in the enum until a matching code is found,
+		 * assumes the given argument is in the valid range of [1, 3].
+		 * 
+		 * @param num the number of the job to find
+		 * @return the job
+		 */
+		public static Job valueOfCode(int num) {
+			Job value = null;
+
+			for (Job current : values()) {
+				if (num == current.getCode()) {
+					value = current;
+					break;
+				}
+			}
+
+			return value;
+		}
 	}
 
 	/** The job worked during the shift */
-	private Jobs job;
+	private Job job;
 
 	/**
-	 * Create a new MarketShift and set its information by calling the parent Shift
-	 * class' constructor.
+	 * Create a new MarketShift and set its information by calling
+	 * the parent Shift class' constructor.
 	 * 
-	 * @param args an array of strings with the different elements each representing
-	 *             a different piece of information about the shift to create
+	 * @param args an array of strings with the different elements
+	 *             each representing a different piece of information about the
+	 *             shift to create
 	 */
 	public MarketShift(String[] args) {
 		super(args);
-
-		int jobChoice = Integer.parseInt(args[4]);
-		switch (jobChoice) {
-			case 1:
-				this.job = Jobs.CASHIER;
-				break;
-			case 2:
-				this.job = Jobs.STOCKER;
-				break;
-			default:
-				this.job = Jobs.UTILITY;
-				break;
-		}
+		this.job = Job.valueOfCode(Integer.parseInt(args[4]));
 	}
 
 	/**
 	 * @return the job worked during this shift
 	 */
-	public Jobs getJob() {
+	public Job getJob() {
 		return job;
 	}
 
