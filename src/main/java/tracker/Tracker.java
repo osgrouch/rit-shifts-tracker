be86b4444ca6cb542011418d421c8afbab369ae1
Oscar <theosgrouch@gmail.com>
2022-05-22 13:56:25 -0400

    Adding rough picocli implementation

--- src/main/java/tracker/Tracker.java
+++ src/main/java/tracker/Tracker.java
@@ -1,2 +1,46 @@
-package tracker;public class Tracker {
+package tracker;
+
+import picocli.CommandLine;
+
+import java.io.PrintStream;
+
+/** The main Shift Tracker application using Picocli */
+@CommandLine.Command (name = "Shift Tracker", version = "0.0", mixinStandardHelpOptions = true)
+public class Tracker implements Runnable {
+	/** Where to write output to */
+	private final PrintStream out;
+
+	/** Name of an XML file representing a PayPeriod to add a shift to */
+	@CommandLine.Option (names = { "-a", "--add-shift" },
+	                     description = "Add a new shift to an existing PayPeriod file.")
+	String addToFilename;
+
+	/** Boolean indicating the creation a new PayPeriod */
+	@CommandLine.Option (names = { "-n", "--new" },
+	                     description = "Create a new PayPeriod and write it to a new file.")
+	boolean newPayPeriod;
+
+	/** Name of an XML file representing a PayPeriod to read the contents of */
+	@CommandLine.Option (names = { "-r", "--read" },
+	                     description = "Read the contents of an existing PayPeriod file.")
+	String readFromFilename;
+
+	/**
+	 * Create a new Tracker instance and set output stream as the console.
+	 */
+	public Tracker () {
+		this.out = System.out;
+	}
+
+	public static void main (String[] args) {
+		int exitCode = new CommandLine(new Tracker()).execute(args);
+		System.exit(exitCode);
+	}
+
+	/**
+	 * Start the thread this application belongs to and print out the app's help message
+	 */
+	@Override public void run () {
+		CommandLine.usage(this, out);
+	}
 }
